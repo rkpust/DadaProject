791e2da22956350eb8e51d66f7ed4e754f3acbf5
rkpust <rezaul.cse.pust17@gmail.com>
2024-10-26 11:17:18 +0600

    added button, search andd table

--- DadaApp/admin.py
+++ DadaApp/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import CapOrder
 
 # Register your models here.
+admin.site.register(CapOrder)
--- DadaApp/views.py
+++ DadaApp/views.py
@@ -1,16 +1,45 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .forms import CapOrderForm
+from .models import CapOrder
 
 # Create your views here.
 def dashboard(request):
-    return render(request, "dashboard.html")
+    results = CapOrder.objects.all()
+    
+
+    if request.method == 'POST':
+        content = request.POST['content']
+        try:
+            s_results = CapOrder.objects.get(ps_number=content)
+        except:
+            try:
+                s_results = CapOrder.objects.get(ps_date=content)
+            except:
+                try:
+                    # s_results = CapOrder.objects.get(byr=content)
+                    pass
+                except:
+                    pass
+
+
+        context = {
+            's_results': s_results,
+            }
+        return render(request, "dashboard.html", context)
+
+
+    
+    context = {
+        'results': results,
+    }
+    return render(request, "dashboard.html", context)
 
 
 def submit_form(request):
     if request.method == 'POST':
         form = CapOrderForm(request.POST)
-        if form.is_valid():
+        if form.is_valid():  
             form.save()
             return redirect('dashboard')  # Redirect after successful submission
     else:
